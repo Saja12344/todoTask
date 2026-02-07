f3dd74be741d4873ed6fe71843876295d351e83c
Shahad <shahad0x66@gmail.com>
2026-02-07 19:29:54 +0300

    Initial Commit

---
+++ todoTask/todoTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoTaskApp.swift
+//  todoTask
+//
+//  Created by شهد عبدالله القحطاني on 19/08/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct todoTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ todoTaskTests/todoTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  todoTaskTests.swift
+//  todoTaskTests
+//
+//  Created by شهد عبدالله القحطاني on 19/08/1447 AH.
+//
+
+import Testing
+@testable import todoTask
+
+struct todoTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
